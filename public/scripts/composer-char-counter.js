44388787b0770e5719042c2bd94c9f22eae4da24
pigbog <mesteves@gmail.com>
2019-01-23 01:32:44 +0000

    Added tweet styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(".new-tweet textarea").on("input", 	function(){
+	$(this).parent().children(".counter").text(140 -$(this).val().length);
+	if ($(this).val().length > 140){
+		$(this).parent().children(".counter").addClass("red");}
+
+	if ($(this).val().length < 140){
+		$(this).parent().children(".counter").removeClass("red")
+	}
+
+});
+
